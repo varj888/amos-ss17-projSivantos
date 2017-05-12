60e4a9345df2abe3a2c3c262366a03ffd6c7acbb
Frueh <frueh90@sieglitzhof.net>
2017-05-12 20:27:28 +0200

    Outsource Request and Serializer

---
+++ raspberry-uc-system/RaspberryUserConstrolSystem/RaspberryBackend/Request.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    /// <summary>
+    /// Unit of transfer by the RequestConnClient Class
+    /// is only as a container for the two variables methodName and parameter
+    /// note: this class uses the default contract namespace
+    /// </summary>
+    [DataContract]
+    public class Request
+    {
+        public Request(string methodName, Object parameter)
+        {
+            this.methodName = methodName;
+            this.parameter = parameter;
+        }
+
+        [DataMember]
+        public string methodName;
+
+        [DataMember]
+        public Object parameter;
+    }
+}
