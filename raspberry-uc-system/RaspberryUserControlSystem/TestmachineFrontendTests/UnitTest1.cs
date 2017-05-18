8aa6174df0605f70cc474b3715a73cde26b3e92c
Julian <Schnei.Julian@gmail.com>
2017-05-18 10:07:00 -0700

    Working Front end unit test project

---
+++ raspberry-uc-system/RaspberryUserControlSystem/TestmachineFrontendTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestmachineFrontend;
+
+namespace TestmachineFrontendTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
