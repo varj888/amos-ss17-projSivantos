36cda1176736752979bc4d976a2e6586247ce78e
varj888 <varjvarj888@gmail.com>
2017-05-21 23:30:31 +0200

    added example NUnit test

---
+++ raspberry-uc-system/RaspberryUserControlSystem/TestmachineFrontendTests1/NUnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace TestmachineFrontendTests1
+{
+    public class NUnitTest1
+    {
+        [TestFixture]
+        public class UnitTest1
+        {
+            [Test]
+            public void TestMethod1()
+            {
+                Assert.AreEqual(1, 1);
+            }
+        }
+    }
+}
