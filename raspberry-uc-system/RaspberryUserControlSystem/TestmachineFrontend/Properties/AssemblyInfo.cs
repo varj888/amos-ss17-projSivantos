839028deffde279d507c0911cd38137ac0a79f4a
varj888 <varjvarj888@gmail.com>
2017-05-21 21:01:27 +0200

    created test project with IntelliTest

--- raspberry-uc-system/RaspberryUserControlSystem/TestmachineFrontend/Properties/AssemblyInfo.cs
+++ raspberry-uc-system/RaspberryUserControlSystem/TestmachineFrontend/Properties/AssemblyInfo.cs
@@ -53,3 +53,7 @@ using System.Windows;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: InternalsVisibleTo("TestmachineFrontend.Tests")]
+[assembly: InternalsVisibleTo("TestmachineFrontend.Explorables")]
+[assembly: InternalsVisibleTo("TestmachineFrontendTests1")]
+
