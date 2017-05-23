43b1d19c9505557a975ef3fe9a4c9a9adf7630aa
Julian <Schnei.Julian@gmail.com>
2017-05-23 13:30:20 -0700

    added a test class for the breadboard factory

---
+++ raspberry-uc-system/RaspberryUserControlSystem/RaspberryBackendTests/Data/BreadboardFactoryTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RaspberryBackend.Data;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+namespace RaspberryBackendTests.Data
+{
+
+    [TestClass]
+    public class BreadboardFactoryTests
+    {
+
+        BreadboardFactory factory;
+
+        [TestInitialize]
+        public void setUp()
+        {
+            factory = new BreadboardFactory();
+        }
+
+        [TestMethod]
+        public void testXmlReader()
+        {
+            Debug.Write(factory.toString());
+        }
+
+        [TestCleanup]
+        public void tearDown()
+        {
+
+        }
+    }
+}
