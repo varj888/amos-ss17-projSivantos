39c1bc2d800f63ef5622fdd1d75d42cfe05411e5
varj888 <varjvarj888@gmail.com>
2017-05-28 14:26:32 +0200

    added class Multiplexer

---
+++ raspberry-uc-system/RaspberryUserControlSystem/RaspberryBackend/Data/Multiplexer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
+using Windows.Devices.I2c;
+
+namespace RaspberryBackend.Data
+{
+    /// <summary>
+    /// represents the physical multiplexer (currently using ADG2128)
+    /// and implements the core functionality of the device
+    /// ADG2128 is an analog cross point switch with an array size of 8 x 12
+    /// </summary>
+    public class Multiplexer
+    {
+        /// <summary>
+        /// total ammount of available switches (8 x 12 --> 96)
+        /// </summary>
+        private readonly int switches = 96;
+
+        //private static I2cInstance;
+
+        public Multiplexer()
+        {
+            //I2cInstance = I2C.getInstance();
+        }
+        /// <summary>
+        /// simultaneously updates all switches
+        /// using the LDSW command
+        /// </summary>
+        public void updateAllSwitches()
+        {
+
+        }
+        /// <summary>
+        /// resets (switch off) all of the
+        /// switch channels
+        /// </summary>
+        public void resetAll()
+        {
+
+        }
+        /// <summary>
+        /// switch on all the channells (switches)
+        /// </summary>
+        public void powerON()
+        {
+
+        }
+    }
+}
