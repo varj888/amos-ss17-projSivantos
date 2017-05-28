45ae96c3e9f0aa22a50087cf3d451efef7b19b37
Tango1266 <Tango1266@arcor.de>
2017-05-28 14:15:11 +0200

    updated testcase for new command

--- raspberry-uc-system/RaspberryUserControlSystem/RaspberryBackendTests/Components/RequestControllerTests.cs
+++ raspberry-uc-system/RaspberryUserControlSystem/RaspberryBackendTests/Components/RequestControllerTests.cs
@@ -9,6 +9,7 @@ namespace RaspberryBackendTests
     [TestClass]
     public class RequestControllerTests
     {
+
         RequestController requestController;
         GPIOinterface inter;
 
@@ -18,6 +19,7 @@ namespace RaspberryBackendTests
             inter = new GPIOinterface();
             //do not init the pins
             requestController = RequestController.Instance;
+
 
         }
 
@@ -40,28 +42,39 @@ namespace RaspberryBackendTests
             Request lightLEDRequest = new Request("LightLED", 1);
             Command lightLEDCommand = requestController.handleRequest(lightLEDRequest);
             Assert.IsTrue(lightLEDCommand is LightLED);
+            Assert.IsTrue(Command.Instances.ContainsValue(lightLEDCommand));
 
             Request readPinRequest = new Request("ReadPin", 1);
             Command readPinCommand = requestController.handleRequest(readPinRequest);
             Assert.IsTrue(readPinCommand is ReadPin);
+            Assert.IsTrue(Command.Instances.ContainsValue(readPinCommand));
 
             Request resetPinRequest = new Request("ResetPin", 2);
             Command resetPinCommand = requestController.handleRequest(resetPinRequest);
             Assert.IsTrue(resetPinCommand is ResetPin);
+            Assert.IsTrue(Command.Instances.ContainsValue(resetPinCommand));
 
             Request writePinRequest = new Request("WritePin", 3);
             Command writePinCommand = requestController.handleRequest(writePinRequest);
             Assert.IsTrue(writePinCommand is WritePin);
+            Assert.IsTrue(Command.Instances.ContainsValue(writePinCommand));
+
 
             Request TurnOnHIRequest = new Request("TurnOnHI", 127);
             Command TurnOnHICommand = requestController.handleRequest(TurnOnHIRequest);
             Assert.IsTrue(TurnOnHICommand is TurnOnHI);
+            Assert.IsTrue(Command.Instances.ContainsValue(TurnOnHICommand));
 
             Request ToggleBacklight_LCDRequest = new Request("ToggleBacklight_LCD", 1);
             Command ToggleBacklight_LCDCommand = requestController.handleRequest(ToggleBacklight_LCDRequest);
             Assert.IsTrue(ToggleBacklight_LCDCommand is ToggleBacklight_LCD);
+            Assert.IsTrue(Command.Instances.ContainsValue(ToggleBacklight_LCDCommand));
 
 
+            Request SendToLCDRequest = new Request("SendToLCD", "test");
+            Command SendToLCDCommand = requestController.handleRequest(SendToLCDRequest);
+            Assert.IsTrue(SendToLCDCommand is SendToLCD);
+            Assert.IsTrue(Command.Instances.ContainsValue(SendToLCDCommand));
 
         }
 
