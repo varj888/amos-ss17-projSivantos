d1e198655fe2e910e15b57da68364935f88c46bf
Tango1266 <Tango1266@arcor.de>
2017-06-07 00:16:31 +0200

    partialised Raspberry Pi class

---
+++ raspberry-uc-system/RaspberryUserControlSystem/RaspberryBackend/Components/RaspberryPi/HIcontrol.cs
@@ -0,0 +1,37 @@
+﻿namespace RaspberryBackend
+{
+    public partial class RaspberryPi
+    {
+        /// <summary>
+        /// usage example:
+        /// turns ADConverter on with
+        /// 3.3V to the channel 2 with DAC voltage 1.5
+        /// </summary>
+        public void turnHI_on(double voltage)
+        {
+            ADCDAC.setDACVoltage(voltage);
+        }
+
+
+        /// <summary>
+        /// Set the potentiometer to a value from 0000 0000 - 0111 1111
+        /// </summary>
+        /// <param name="data"></param>
+        public void setAnalogVolume(byte[] data)
+        {
+            Potentiometer.write(data);
+        }
+
+        /// <summary>
+        /// Connect pins x to y on the multiplexer. Right now this is the same as _multiplexer.connectPins except no checks
+        /// are performed on the input parameters. Eventually we can check for success right here.
+        /// </summary>
+        /// <param name="xPin"></param>
+        /// <param name="yPin">/param>
+        public void connectPins(int xPin, int yPin)
+        {
+            Multiplexer.connectPins(xPin, yPin);
+        }
+    }
+
+}
---
+++ raspberry-uc-system/RaspberryUserControlSystem/RaspberryBackend/Components/RaspberryPi/LCDcontrol.cs
@@ -0,0 +1,43 @@
+﻿namespace RaspberryBackend
+{
+    public partial class RaspberryPi
+    {
+        /// <summary>
+        /// Print string to LCD display
+        /// </summary>
+        /// <param name="s"></param>
+        public void writeToLCD(string s)
+        {
+            LCD.clrscr();
+            LCD.prints(s);
+        }
+
+        /// <summary>
+        /// Print two lines to LCD
+        /// </summary>
+        /// <param name="s"></param>
+        public void writeToLCDTwoLines(string s)
+        {
+            int maxCharLCD = 16;
+            LCD.printInTwoLines(s, maxCharLCD);
+        }
+
+        /// <summary>
+        /// Reset the LCD (clear it's screen)
+        /// </summary>
+        public void resetLCD()
+        {
+            LCD.initiate();
+        }
+
+        /// <summary>
+        /// Set state for background in LCD. Will want to switch to toggle
+        /// </summary>
+        /// <param name="targetState"></param>
+        public void setLCDBackgroundState(byte targetState)
+        {
+            LCD.backLight = targetState;
+            LCD.write(targetState, 1);
+        }
+    }
+}
