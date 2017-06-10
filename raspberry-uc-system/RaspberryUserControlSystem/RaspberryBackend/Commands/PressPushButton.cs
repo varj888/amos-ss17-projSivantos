0a26d793de4027d7e7adf118d59d4260eb21b7b2
Florian Rühland <florian.ruehland@fau.de>
2017-06-10 13:47:48 +0200

    Add command PressPushButton

---
+++ raspberry-uc-system/RaspberryUserControlSystem/RaspberryBackend/Commands/PressPushButton.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Windows.Devices.Gpio;
+
+namespace RaspberryBackend
+{
+    /// <summary>
+    /// This class represents a Command. It it can be used to read a spefic gpio pin of the RaspberryPi. 
+    /// </summary>
+    class PressPushButton : Command
+    {
+        UInt16 pushButton_Pin = 21;
+
+        public PressPushButton(RaspberryPi raspberryPi) : base(raspberryPi)
+        {
+        }
+
+        /// <summary>
+        /// executes the Command ReadPin 
+        /// </summary>
+        /// <param name="parameter">represents the GpioPin to read from</param>
+        public override void executeAsync(Object parameter)
+        {
+            UInt16 duration = (UInt16)parameter;
+            RaspberryPi.activatePin(pushButton_Pin);
+            Task.Delay(duration).Wait();
+            RaspberryPi.deactivatePin(pushButton_Pin);
+        }
+    }
+}
