01862dbb66aca5cf679354105cfa9f1ff4c63245
Florian Rühland <florian.ruehland@fau.de>
2017-06-18 12:21:28 +0200

    Add commands for new API features and implement them in raspberry class. This implements feature ID #17, 18

---
+++ raspberry-uc-system/RaspberryUserControlSystem/RaspberryBackend/RaspberryPiRemoteAPI/DisableAudioShoe.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+
+namespace RaspberryBackend
+{
+    /// <summary>
+    /// This class represents a Command. It it can be used to deactivate an audio-shoe.
+    /// </summary>
+    public partial class RaspberryPi
+    {
+
+        /// <summary>
+        /// executes the Command EnableAudioShoe.
+        /// </summary>
+        public void DisableAudioShoe()
+        {
+            this.unsetAudioShoe();
+            Debug.Write("Unset audio-shoe");
+        }
+    }
+}
---
+++ raspberry-uc-system/RaspberryUserControlSystem/RaspberryBackend/RaspberryPiRemoteAPI/EnableTeleCoil.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+
+namespace RaspberryBackend
+{
+    /// <summary>
+    /// This class represents a Command. It it can be used to deactivate a tele-coil.
+    /// </summary>
+    public partial class RaspberryPi
+    {
+
+        /// <summary>
+        /// executes the Command EnableTeleCoil.
+        /// </summary>
+        public void EnableTeleCoil()
+        {
+            this.setTeleCoil();
+            Debug.Write("Set telecoil");
+        }
+    }
+}
