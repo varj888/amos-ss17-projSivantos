4742b5a4c060a12fc6d02d61e38976dcc24bb394
Florian Rühland <florian.ruehland@fau.de>
2017-06-18 11:02:59 +0200

    Fix LCD-display by sending ints from now on.

--- raspberry-uc-system/RaspberryUserControlSystem/RaspberryBackend/RaspberryPiRemoteAPI/ToggleBacklight_LCD.cs
+++ raspberry-uc-system/RaspberryUserControlSystem/RaspberryBackend/RaspberryPiRemoteAPI/ToggleBacklight_LCD.cs
@@ -13,18 +13,18 @@ namespace RaspberryBackend
         /// Toogles the Backlight of the LCD to onn or off
         /// </summary>
         /// <param name="parameter">1 for Backlight on or 0 for off</param>
-        public void ToggleBacklight_LCD(string requestedParameter)
+        public void ToggleBacklight_LCD(int requestedParameter)
         {
             const byte ON = 0x01;
             const byte OFF = 0x00;
 
-            if (requestedParameter.Equals("1"))
+            if (requestedParameter.Equals(1))
             {
                 Debug.WriteLine("Received command ToggleBacklightLCD On!");
                 switchToState(ON);
 
             }
-            else if (requestedParameter.Equals("0"))
+            else if (requestedParameter.Equals(0))
             {
                 Debug.WriteLine("Received command ToggleBacklightLCD Off!");
                 switchToState(OFF);
