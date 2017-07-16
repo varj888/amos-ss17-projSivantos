aa133bda34f952a6ecf2ea21ef96528d48bc904d
Adrian MK <adrianmkofficial@me.com>
2017-07-17 00:06:38 +0200

    Added About Window (not yet configured to open when "?"-Button is clicked because MVVM doesn't allow for an easy way to open new windows apparently)

---
+++ UserAgent/TestMachineFrontend1/ViewModel/AboutViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMachineFrontend1.ViewModel
+{
+    class AboutViewModel
+    {
+        public string Version
+        {
+            get { return "Version " + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString(); }
+        }
+    }
+}
