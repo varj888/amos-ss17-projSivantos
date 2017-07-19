5f47ce3058362f361a6748cbfc89c4bee83646de
Adrian MK <adrianmkofficial@me.com>
2017-07-19 13:47:35 +0200

    Add About Window Added simple About Window showcasing the developers of the project. Used a click-event for implementation instead of a WindowFactory (which would have been more appropriate but out of the scope for this frontend currently).

---
+++ UserAgent/ProductionFrontend/ViewModel/AboutViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestMachineFrontend1.Helpers;
+
+namespace TestMachineFrontend1.ViewModel
+{
+    class AboutViewModel : ObservableObject
+    {
+        public string Version
+        {
+            get { return System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString(); }
+        }
+    }
+}
